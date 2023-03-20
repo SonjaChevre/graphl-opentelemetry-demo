88c7fde8a9034c77315a1aadfdab3a2175006823
Sonja Chèvre <sonja+github@tyk.io>
2023-03-20 12:39:15 +0100

    traces are send to Jaeger

--- frontend/last-minute-deals/src/tracer.js
+++ frontend/last-minute-deals/src/tracer.js
@@ -6,29 +6,48 @@ import { UserInteractionInstrumentation } from '@opentelemetry/instrumentation-u
 import { XMLHttpRequestInstrumentation } from '@opentelemetry/instrumentation-xml-http-request';
 import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
-
-const setupTracer = () => {
-    const provider = new WebTracerProvider();
-
-    provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
-    
-    provider.register({
-      // Changing default contextManager to use ZoneContextManager - supports asynchronous operations - optional
-      contextManager: new ZoneContextManager(),
-    });
-    
-    // Registering instrumentations
-    registerInstrumentations({
-      instrumentations: [
-        new DocumentLoadInstrumentation(),
-        new UserInteractionInstrumentation(),
-        new XMLHttpRequestInstrumentation(),
-        new FetchInstrumentation()
-      ],
-    });
-    
-}
+import { Resource } from '@opentelemetry/resources';
+import { OTLPTraceExporter  } from "@opentelemetry/exporter-trace-otlp-http";
 
 
-setupTracer();
+const collectorOptions = {
+    url: "http://localhost:4318/v1/traces", 
+    headers: {
+    "Content-Type": "application/json", 
+    'Access-Control-Allow-Headers': '*',
+    'X-CSRF': '1',
+  },
+    concurrencyLimit: 10,
+  };
+
+
+// Trace provider (Main aplication trace)
+const provider = new WebTracerProvider({
+    resource: new Resource({
+      "service.name": "Frontend",
+    }
+    )});
+
+// Exporter (opentelemetry collector hidden behind bff proxy)
+const exporter = new OTLPTraceExporter (collectorOptions);
+
+// Instrumentation configurations for frontend
+const fetchInstrumentation = new FetchInstrumentation({
+    ignoreUrls : ["https://some-ignored-url.com"]
+  });
+  
+  fetchInstrumentation.setTracerProvider(provider);
+  
+  provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+  
+  provider.register({
+    contextManager: new ZoneContextManager(),
+  });
+  
+  // Registering instrumentations
+  registerInstrumentations({
+      instrumentations: [new FetchInstrumentation()],
+  });
+  
+
 
