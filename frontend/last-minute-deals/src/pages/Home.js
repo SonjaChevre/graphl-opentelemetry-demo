830a26e78de59297d8fdbae46dfd75278591fbe8
Sonja Chèvre <sonja+github@tyk.io>
2023-03-23 13:16:32 +0100

    react app calls the graphql server
    and no longer use external graphql trevorblade. setting cors also.

--- frontend/last-minute-deals/src/pages/Home.js
+++ frontend/last-minute-deals/src/pages/Home.js
@@ -4,31 +4,30 @@ import { useQuery, gql } from '@apollo/client';
 
 
 const GET_COUNTRY = gql`
+query {
   
- query GetCountries {
-  countries {
+  books {
+    id
     name
-capital
-emoji
+    authors {
+      id
+      name
+    }
   }
 }
+
 `;
 
 
-function DisplayCountry() {
+function DisplayBooks() {
   const { loading, error, data } = useQuery(GET_COUNTRY);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
 
-  return data.countries.map(({ name, capital, emoji }) => (
-    <div key="{code}">
-      <h3>{emoji} {name}</h3>
-      <p>
-      <b>Capital: </b>
-      {capital}
-      </p>
-      <br />
+  return data.books.map(({ id, name}) => (
+    <div key="{id}">
+      <h3>{name}</h3>
     </div>
   ));
 }
@@ -36,11 +35,11 @@ function DisplayCountry() {
 function Home() {
   return (
       <div>
-        <h2>Last-Minute Deals 🏖️</h2>
+        <h2>Books</h2>
         <br/>
-        <DisplayCountry />
+        <DisplayBooks />
       </div>
   );
 }
 
-export default Home;
+export default DisplayBooks;
